95c09c24545034a8bbc9c654250021b8cd667075
Julian <julyysanchez@hotmail.com>
2021-05-26 23:30:03 -0300

    1 - Spring data JPA config 2 - Spring data JPA table creations 3 - Spring data JPA repositorys creation

---
+++ src/main/java/com/emcove/rest/api/Core/repository/EntreprenuershipRepository.java
@@ -0,0 +1,7 @@
+package com.emcove.rest.api.Core.repository;
+
+import com.emcove.rest.api.Core.response.Entreprenuership;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntreprenuershipRepository extends JpaRepository<Entreprenuership,Integer> {
+}
