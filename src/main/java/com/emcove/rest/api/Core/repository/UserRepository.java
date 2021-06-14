ba58f2c19f06d61a1431bbe8e92971c7f4ad49fe
mariano-per <marianoo.molina@gmail.com>
2021-06-14 18:57:17 -0300

    Remuevo transactional de UserRepository

--- src/main/java/com/emcove/rest/api/Core/repository/UserRepository.java
+++ src/main/java/com/emcove/rest/api/Core/repository/UserRepository.java
@@ -3,12 +3,10 @@ package com.emcove.rest.api.Core.repository;
 import com.emcove.rest.api.Core.response.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Boolean existsByUsername(String username);
     Optional<User> findByUsername(String username);
-    @Transactional
     Optional<User> findByEntrepreneurshipId(Integer id);
 }
