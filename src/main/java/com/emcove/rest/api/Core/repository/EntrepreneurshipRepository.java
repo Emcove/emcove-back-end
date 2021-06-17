b040671da912edead972c7773dda758e12181e95
Julian <julyysanchez@hotmail.com>
2021-06-16 23:55:22 -0300

    Adds entrepreneurship frontend connection

--- src/main/java/com/emcove/rest/api/Core/repository/EntrepreneurshipRepository.java
+++ src/main/java/com/emcove/rest/api/Core/repository/EntrepreneurshipRepository.java
@@ -2,6 +2,8 @@ package com.emcove.rest.api.Core.repository;
 
 import com.emcove.rest.api.Core.response.Entrepreneurship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface EntrepreneurshipRepository extends JpaRepository<Entrepreneurship,Integer> {
 }
--- src/main/java/com/emcove/rest/api/Core/repository/ProductRepository.java
+++ src/main/java/com/emcove/rest/api/Core/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.emcove.rest.api.Core.repository;
 import com.emcove.rest.api.Core.response.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
